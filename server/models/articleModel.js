03053b157e5548149a6be29c95ae66a57f0ddcba
Ramith-D-Rodrigo <ramithrodrigo@yahoo.com>
2022-10-23 23:09:54 +0530

    edit article model

--- server/models/articleModel.js
+++ server/models/articleModel.js
@@ -5,7 +5,7 @@ const articleSchema = mongoose.Schema({
     title : String,
     content : String,
     author : {type: mongoose.Schema.Types.ObjectId, ref: 'authors'},
-    comments : {type: mongoose.Schema.Types.ObjectId, ref: 'authors'},
+    comments : [{type: mongoose.Schema.Types.ObjectId, ref: 'comments'}],
     likes : [{type: mongoose.Schema.Types.ObjectId, ref: 'authors'}],
 }, {timestamps : true})
 
