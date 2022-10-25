a70a910fa0791f0242425ec059f43326c783da6d
Charith Jayarangana <cj.ptsz@yahoo.com>
2022-10-25 23:54:12 +0530

    update article.js & add some logic

--- server/routes/article.js
+++ server/routes/article.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/articleModel");
 const Author = require("../models/authorModel");
+const asyncHandler = require("express-async-handler");
 
 router.get("/",(req, res)=>{    //get all articles
     Article.find({}, (err, result) => {
@@ -37,13 +38,22 @@ router.get("/authors/:id",(req, res)=>{ //get articles of a certain author
     });
 })
 
-router.post("/:auid",async (req, res)=>{  //post article of a certain author
+router.post("/:auid",asyncHandler( async (req, res)=>{  //post article of a certain author
     let title = req.body.title;
     let content = req.body.content;
     let authorID = req.params.auid;
     let updated = false;
 
+    if(!title || !content ){
+        res.status(400);
+        throw new Error('Please fill all the required fields');
+    }
+
     const authorRes = await Author.findOne({_id: authorID}, {name : 1});
+    if(!authorRes){
+        res.status(400);
+        throw new Error('Not found Author');
+    }
     const author = authorRes.name;
     
     let newArticle = {title, content, author, updated};
@@ -57,10 +67,15 @@ router.post("/:auid",async (req, res)=>{  //post article of a certain author
             res.json({status: 200, msg: "Post Created Successfully!", data: result});
         }
     });
-})
+}));
 
-router.put("/:id", (req,res)=>{ //update article
+router.put("/:id", asyncHandler( async (req,res)=>{ //update article
     let updatedContent = req.body.updatedcontent;
+
+    if(!updatedContent){
+        res.status(400);
+        throw new Error('Please fill all fields');
+    }
 
     Article.findByIdAndUpdate(req.params.id, {content : updatedContent, updated: true}, (err, result) =>{
         if(err){
@@ -70,7 +85,7 @@ router.put("/:id", (req,res)=>{ //update article
             res.send(result);
         }
     })
-})
+}));
 
 router.delete("/:id", (req,res)=>{  //delete article
     Article.findByIdAndDelete(req.params.id, (err)=>{
